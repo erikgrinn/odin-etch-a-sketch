96e601556a78fb3a59fa3a782f8e61b254f13711
Erik Grinn <grinn.erik@gmail.com>
2024-08-18 14:50:54 -0400

    centered and numbered grid with flex

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 const gridContainer = document.querySelector('div.grid-container')
 
+const gridColTemplate = document.createElement('div')
+gridColTemplate.classList.add('grid-col')
+
 const gridItemTemplate = document.createElement('div')
 gridItemTemplate.classList.add('grid-item')
-gridItemTemplate.textContent = 'i'
 
-for (let i=0; i<256; i++) {
-    let gridItem = gridItemTemplate.cloneNode(true)
-    gridContainer.appendChild(gridItem)
-};
+let count = 0
+
+for (let i=0; i<16; i++) {
+    let gridCol = gridColTemplate.cloneNode()
+    gridContainer.appendChild(gridCol)
+    for (let j=0; j<16; j++) {
+        count++
+        let gridItem = gridItemTemplate.cloneNode()
+        gridItem.textContent = count
+        gridCol.appendChild(gridItem)
+    };
+}
