51522651bfe5d299c083e0542b6e2084edbc1477
Erik Grinn <grinn.erik@gmail.com>
2024-08-18 15:59:40 -0400

    maybe need to use <form> to get user input

--- main.js
+++ main.js
@@ -6,20 +6,40 @@ gridColTemplate.classList.add('grid-col')
 const gridItemTemplate = document.createElement('div')
 gridItemTemplate.classList.add('grid-item')
 
-let count = 0
-
 for (let i=0; i<16; i++) {
     let gridCol = gridColTemplate.cloneNode()
     gridContainer.appendChild(gridCol)
     for (let j=0; j<16; j++) {
-        count++
         let gridItem = gridItemTemplate.cloneNode()
-        gridItem.textContent = count
         gridItem.addEventListener('mouseover', handleHover)
         gridCol.appendChild(gridItem)
     };
 }
 
+function resizeGrid (resizeInput) {
+    for (let i=0; i<resizeInput; i++) {
+        let gridCol = gridColTemplate.cloneNode()
+        gridContainer.appendChild(gridCol)
+        for (let j=0; j<resizeInput; j++) {
+            let gridItem = gridItemTemplate.cloneNode()
+            gridItem.addEventListener('mouseover', handleHover)
+            gridCol.appendChild(gridItem)
+        };
+    }
+}
+
 function handleHover (event) {
     event.target.style.backgroundColor = 'blue'
 }
+
+// function handleClick (event) {
+
+// }
+
+let resizeInput = document.querySelector('.resizeInput').value
+
+const resizeButton = document.querySelector('button')
+// resizeButton.addEventListener('click', handleClick)
+
+console.log(resizeInput)
+
