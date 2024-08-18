620246d003b0ee9184a6895ca5d829ed2d9d3224
Erik Grinn <grinn.erik@gmail.com>
2024-08-18 17:10:06 -0400

    need to eliminate fixed size, implement flex item sizing

--- main.js
+++ main.js
@@ -6,21 +6,16 @@ gridColTemplate.classList.add('grid-col')
 const gridItemTemplate = document.createElement('div')
 gridItemTemplate.classList.add('grid-item')
 
-for (let i=0; i<16; i++) {
-    let gridCol = gridColTemplate.cloneNode()
-    gridContainer.appendChild(gridCol)
-    for (let j=0; j<16; j++) {
-        let gridItem = gridItemTemplate.cloneNode()
-        gridItem.addEventListener('mouseover', handleHover)
-        gridCol.appendChild(gridItem)
-    };
-}
+let resizeNum = 16
 
-function resizeGrid (resizeInput) {
-    for (let i=0; i<resizeInput; i++) {
+resizeGrid(resizeNum)
+
+function resizeGrid (resizeNum) {
+    gridContainer.innerHTML = ''
+    for (let i=0; i<resizeNum; i++) {
         let gridCol = gridColTemplate.cloneNode()
         gridContainer.appendChild(gridCol)
-        for (let j=0; j<resizeInput; j++) {
+        for (let j=0; j<resizeNum; j++) {
             let gridItem = gridItemTemplate.cloneNode()
             gridItem.addEventListener('mouseover', handleHover)
             gridCol.appendChild(gridItem)
@@ -32,14 +27,19 @@ function handleHover (event) {
     event.target.style.backgroundColor = 'blue'
 }
 
-// function handleClick (event) {
+function handleClick (event) {
+    resizeNum = Number(prompt('Enter a number <= 100, and a new square grid will be created'))
+    if (resizeNum > 0 && resizeNum <= 100) {
+        resizeGrid(resizeNum);
+    } else {
+        alert('Please enter a valid number between 1 and 100');
+        handleClick()
+    }
+}
 
-// }
-
-let resizeInput = document.querySelector('.resizeInput').value
 
 const resizeButton = document.querySelector('button')
-// resizeButton.addEventListener('click', handleClick)
+resizeButton.addEventListener('click', handleClick)
 
-console.log(resizeInput)
+console.log(resizeNum)
 
